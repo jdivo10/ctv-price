fc814f00f00dbc9fd478941f0aae0f3bc3d41af6
jdivo10 <17180522+jdivo10@users.noreply.github.com>
2023-07-25 15:48:13 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { BlockfrostAdapter, NetworkId } from "@minswap/blockfrost-adapter";
+
+const app = express();
+const api = new BlockfrostAdapter({
+    projectId: "mainnet6qOtWugv56QPxT3x0H1jYfX1feK5N3do",
+    networkId: NetworkId.MAINNET,
+});
+
+app.get('/prices', async(req, res) => {
+    let prices;
+    let poolId;
+    for (let i = 1;; i++) {
+        const pools = await api.getPools({
+            page: i,
+            poolAddress: "addr1z8snz7c4974vzdpxu65ruphl3zjdvtxw8strf2c2tmqnxz2j2c79gy9l76sdg0xwhd7r0c0kna0tycz4y5s6mlenh8pq0xmsha",
+        });
+        if (pools.length === 0) {
+            // last page
+            break;
+        }
+        const minADAPool = pools.find(
+            (p) =>
+            p.assetA === "lovelace" &&
+            p.assetB ===
+            "9f452e23804df3040b352b478039357b506ad3b50d2ce0d7cbd5f806435456"
+        );
+        if (minADAPool) {
+            const [a, b] = await api.getPoolPrice({ pool: minADAPool });
+            prices = {
+                ADA_CTV_price: a.toString(),
+                CTV_ADA_price: b.toString()
+            };
+            poolId = minADAPool.id;
+            break;
+        }
+    }
+    res.json({
+        prices,
+        poolId
+    });
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
