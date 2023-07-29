b2750f7fbceffe8f75aa83901c022e08188375db
jdivo10 <17180522+jdivo10@users.noreply.github.com>
2023-07-29 14:14:52 -0400

    Limit API Calls

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 import express from 'express';
 import cors from 'cors';
 import { BlockfrostAdapter, NetworkId } from "@minswap/blockfrost-adapter";
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 // Set up CORS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST'],
-    allowedHeaders: '*'
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 
 const api = new BlockfrostAdapter({
     projectId: "mainnetzFTkLDTPNXT37SkNra7fGS4xjWwRVldY",
     networkId: NetworkId.MAINNET,
 });
 
-app.get('/', async(req, res) => {
+app.get('/prices', async(req, res) => {
+    const filePath = path.resolve(__dirname, 'lastPrice.json');
+
+    if (fs.existsSync(filePath)) {
+        const rawData = fs.readFileSync(filePath);
+        const lastPriceData = JSON.parse(rawData);
+
+        const lastPriceTime = new Date(lastPriceData.timeStamp);
+        const now = new Date();
+
+        const difference = now.getTime() - lastPriceTime.getTime();
+        const differenceInMinutes = Math.floor(difference / 1000 / 60);
+
+        if (differenceInMinutes < 1) {
+            return res.json(lastPriceData);
+        }
+    }
+
     let prices;
     let poolId;
     for (let i = 1;; i++) {
@@ -44,10 +68,15 @@ app.get('/', async(req, res) => {
             break;
         }
     }
-    res.json({
+
+    const newPriceData = {
         prices,
-        poolId
-    });
+        poolId,
+        timeStamp: new Date().toISOString(),
+    };
+
+    fs.writeFileSync(filePath, JSON.stringify(newPriceData, null, 2));
+    res.json(newPriceData);
 });
 
 app.listen(process.env.PORT || 3000)
